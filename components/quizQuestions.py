17e9c413f3b84b6f57215c6b344e6b71bb9cfd32
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2021-12-07 13:51:00 -0500

    adding the base

---
+++ components/quizQuestions.py
@@ -0,0 +1,61 @@
+questions = {
+    "q1" : {
+        "question": "Do you live on earth? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+    "q2" : {
+        "question" : "Are you human? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+    "q3" : {
+        "question" : "do you have super powers? ",
+        "yes" : 10,
+        "no" : 0
+    },
+
+    "q4" : {
+        "question" : "Do you wear a custom suit? ",
+        "yes" : 10,
+        "no" : 0
+    },
+
+    "q5" : {
+        "question" : "Can you fly? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+    "q6" : {
+        "question" : "Are you human? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+    "q7" : {
+        "question" : "Are you human? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+    "q8" : {
+        "question" : "Are you human? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+    "q9" : {
+        "question" : "Are you human? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+    "q10" : {
+        "question" : "Are you human? ",
+        "yes" : 0,
+        "no" : 10
+    }
+}
---
+++ components/vars.py
@@ -0,0 +1,4 @@
+quizTotal = 0
+
+characters = ["Hulk", "Spider-Man", "Iron-Man", "Deadpool"]
+images = ["hulk.jpg", "spiderman.jpg", "ironman.jpg", "deadpool.jpg"]
