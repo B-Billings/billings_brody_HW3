91b79f65cf7eebc27f1bf6d1fcd1ed569abf360b
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2021-12-07 22:08:46 -0500

    got my pillow working properly

--- components/quizTally.py
+++ components/quizTally.py
@@ -1,30 +1,32 @@
 from components import vars
-
-
+from PIL import Image
+hulk = Image.open("hulk.jpeg")
+spider = Image.open("spiderman.jpeg")
+iron = Image.open("ironman.jpeg")
+pool = Image.open("deadpool.jpeg")
 def total(value):
     # do some logic to see which character you selected
-
     if value <= 40:
         vars.character = vars.characters[0]
-
+        hulk.show()
         print("It's " + vars.character)
         # add some emoji icons, or show the character image using the Pillow package
 
     if value == 80:
         vars.character = vars.characters[1]
-
+        spider.show()
         print("It's " + vars.character)
         # add some emoji icons, or show the character image using the Pillow package
 
     if value == 50:
         vars.character = vars.characters[2]
-
+        iron.show()
         print("It's " + vars.character)
         # add some emoji icons, or show the character image using the Pillow package 
     
     if value == 51:
         vars.character = vars.characters[3]
-
+        pool.show()
         print("It's " + vars.character)
         # add some emoji icons, or show the character image using the Pillow package   
         
--- quiz.py
+++ quiz.py
@@ -1,12 +1,10 @@
 from components.quizQuestions import questions
 from components import vars, quizTally, gameRules
-from PIL import Image
 
 while vars.player is False:
-    vars.player = input("Type Yes to begin the quiz")
-   
-    print("==============================================================")
     
     gameRules.gameRules()
+
+
 
     vars.player=False
