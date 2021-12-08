03b64fb87db5a8afbdadebdea1c12be83c97da49
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2021-12-08 17:10:43 -0500

    added some colour

--- components/vars.py
+++ components/vars.py
@@ -1,6 +1,6 @@
 quizTotal = 0
 
-characters = ["Hulk", "Spider-Man", "Iron-Man", "Deadpool"]
-images = ["hulk.jpg", "spiderman.jpg", "ironman.jpg", "deadpool.jpg"]
+characters = ["Hulk", "Spider-Man", "Iron-Man", "Deadpool", "storm"]
+images = ["hulk.jpg", "spiderman.jpg", "ironman.jpg", "deadpool.jpg", "storm.jpeg"]
 
 player=False
