babb452987f0a59046cd650f504539f544fe1704
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2021-12-09 10:08:23 -0500

    fixed some code and tries to add a filter for non y,n choices

--- components/gameRules.py
+++ components/gameRules.py
@@ -1,62 +1,67 @@
 from components.quizQuestions import questions
 from components import vars, quizTally
 from PIL import Image
+from colorama.ansi import Fore, Back, Style
+
 
 def gameRules():
-    
-    answer1 = questions["q1"][input(questions["q1"]["question"])]
-    
+    print (Fore.RED)
+    answer1= questions["q1"][input(questions["q1"]["question"])]
 
     vars.quizTotal += answer1
-    print("==========================================\n")
+    print (Fore.WHITE + "==========================================\n")
 
+    print (Fore.RED)
     answer2 = questions["q2"][input(questions["q2"]["question"])]
     
 
     vars.quizTotal += answer2
-    print("==========================================\n")
+    print (Fore.WHITE + "==========================================\n")
 
-
+    print (Fore.RED)
     answer3 = questions["q3"][input(questions["q3"]["question"])]
     
-
     vars.quizTotal += answer3
-    print("==========================================\n")
-
+    print (Fore.WHITE + "==========================================\n")
+    
+    print (Fore.RED)
     answer4 = questions["q4"][input(questions["q4"]["question"])]
 
     vars.quizTotal += answer4
-    print("==========================================\n") 
-
+    print (Fore.WHITE + "==========================================\n") 
+    print (Fore.RED)
     answer5 = questions["q5"][input(questions["q5"]["question"])]
     
 
     vars.quizTotal += answer5
-    print("==========================================\n")
-
+    print (Fore.WHITE + "==========================================\n")
+    print (Fore.RED)
     answer6 = questions["q6"][input(questions["q6"]["question"])]
     
 
     vars.quizTotal += answer6
-    print("==========================================\n") 
+    print (Fore.WHITE + "==========================================\n") 
 
+    print (Fore.RED)
     answer7 = questions["q7"][input(questions["q7"]["question"])]
     
 
     vars.quizTotal += answer7
-    print("==========================================\n")
+    print (Fore.WHITE + "==========================================\n")
 
+    print (Fore.RED)
     answer8 = questions["q8"][input(questions["q8"]["question"])]
     
 
     vars.quizTotal += answer8
-    print("==========================================\n")
-
+    print (Fore.WHITE + "==========================================\n")
+    
+    print (Fore.RED)
     answer9 = questions["q9"][input(questions["q9"]["question"])]
     
 
     vars.quizTotal += answer9
-    print("==========================================\n")
+    print (Fore.WHITE + "==========================================\n")
 
 
     quizTally.total(vars.quizTotal)
@@ -71,6 +76,6 @@ def gameRules():
         print("Thanks for playing see you again soon!")
         quit()
 
-    print("==============================================================")
+    print (Fore.WHITE + "==========================================\n")
 
 
